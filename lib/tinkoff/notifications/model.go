81749ea61e2422d88103663c34b3cd677e5ed2ba
Dmitriy Papkov <papkovda@me.com>
2023-08-21 11:28:42 +0300

    Fix model.go

--- lib/tinkoff/notifications/model.go
+++ lib/tinkoff/notifications/model.go
@@ -7,22 +7,24 @@ type Item interface {
 }
 
 type PaymentItem struct {
-	TerminalKey  string                 // Идентификатор терминала выданный банком
-	OrderID      string                 // Номер заказа в системеп продавци
-	Success      bool                   // Успешность проведения операции
-	Status       string                 // Статус платежа
-	PaymentID    string                 // Идентификатор транзакции в системе банка
-	ErrorCode    string                 // Код ошибки, 0 - если успешно
-	Amount       int64                  // Сумма платежа в копейках
-	CardId       int64                  // Идентификатор привязанной карты
-	Pan          string                 // Зфмфскированный номер карты
-	ExpDate      string                 // Срок действия карты
-	RebillId     string                 // идентификатор для рекуррентного платежа
-	Token        string                 // Подпись запроса
-	DATA         map[string]interface{} // Дополнительные параметры запроса
-	Route        string                 `json:"Route,omitempty"`        // Способ патежа
-	Source       string                 `json:"Source"`                 // Источник платежа
-	CreditAmount int64                  `json:"CreditAmount,omitempty"` // Сумма выданного кредита в копейках
+	TerminalKey string // Идентификатор терминала выданный банком
+	Success     bool   // Успешность проведения операции
+	Status      string // Статус платежа
+	Token       string // Подпись запроса
+
+	PaymentID        string                 `json:"PaymentId,omitempty"`        // Идентификатор транзакции в системе банка
+	ErrorCode        string                 `json:"ErrorCode,omitempty"`        // Код ошибки, 0 - если успешно
+	Amount           int64                  `json:"Amount,omitempty"`           // Сумма платежа в копейках
+	CardId           int64                  `json:"CardId,omitempty"`           // Идентификатор привязанной карты
+	Pan              string                 `json:"Pan,omitempty"`              // Зфмфскированный номер карты
+	ExpDate          string                 `json:"ExpDate,omitempty"`          // Срок действия карты
+	RebillId         string                 `json:"RebillId,omitempty"`         // идентификатор для рекуррентного платежа
+	OrderID          string                 `json:"OrderId,omitempty"`          // Номер заказа в системеп продавци
+	DATA             map[string]interface{} `json:"Data,omitempty"`             // Дополнительные параметры запроса
+	Route            string                 `json:"Route,omitempty"`            // Способ патежа
+	Source           string                 `json:"Source,omitempty"`           // Источник платежа
+	CreditAmount     int64                  `json:"CreditAmount,omitempty"`     // Сумма выданного кредита в копейках
+	SpAccumulationId string                 `json:"SpAccumulationId,omitempty"` // Accumulation ID
 }
 
 func (p PaymentItem) GetTerminalKey() string {
@@ -44,15 +46,14 @@ type CardItem struct {
 	Status      string // Статус платежа ["COMPLETED", "REJECTED"]
 	Token       string // Подпись запроса
 
-	CustomerKey      string `json:"CustomerKey,omitempty"`      // Идентификатор пользователя
-	RequestKey       string `json:"RequestKey,omitempty"`       // Идентификатор запроса
-	PaymentId        string `json:"PaymentId,omitempty"`        // Идентификатор транзакции в системе банка
-	ErrorCode        string `json:"ErrorCode,omitempty"`        // Код ошибки, 0 - если успешно
-	CardId           int64  `json:"CardId,omitempty"`           // Идентификатор карты в системе банка
-	Pan              string `json:"Pan,omitempty"`              // Зашифрованный номер карты
-	ExpDate          string `json:"ExpData,omitempty"`          // Срок действия карты
-	RebillId         string `json:"RebillId,omitempty"`         // Идентификатор для рекуррентного платежа
-	SpAccumulationId string `json:"SpAccumulationId,omitempty"` // Accumulation ID
+	CustomerKey string `json:"CustomerKey,omitempty"` // Идентификатор пользователя
+	RequestKey  string `json:"RequestKey,omitempty"`  // Идентификатор запроса
+	PaymentId   string `json:"PaymentId,omitempty"`   // Идентификатор транзакции в системе банка
+	ErrorCode   string `json:"ErrorCode,omitempty"`   // Код ошибки, 0 - если успешно
+	CardId      int64  `json:"CardId,omitempty"`      // Идентификатор карты в системе банка
+	Pan         string `json:"Pan,omitempty"`         // Зашифрованный номер карты
+	ExpDate     string `json:"ExpData,omitempty"`     // Срок действия карты
+	RebillId    string `json:"RebillId,omitempty"`    // Идентификатор для рекуррентного платежа
 }
 
 func (c CardItem) GetTerminalKey() string {
